eb52fe2432a2e89b4778cb27fcc9c69d2e0d0aa4
brent <brent.e.meyer@gmail.com>
2019-10-09 12:44:18 -0400

    adds cors package and set up

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
 
 app.disable('x-powered-by');
 
@@ -13,6 +14,8 @@ app.use(morgan('dev'));
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(cors());
 
 app.use('/api/v1', require('./routes'));
 
